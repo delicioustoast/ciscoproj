98da74533efe0919518b8c7d82c52993875c11fd
delicioustoast <ediwhang@gmail.com>
2015-11-18 00:28:02 -0800

    Localized time to PST, added most popular subreddits list and error handling, and improved frontend

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 from flask import Flask, request, url_for, render_template, redirect, flash
 import models
-import urllib, json
+from datetime import datetime
+import praw
+# import urllib, json
 app = Flask(__name__)
 
 @app.route('/')
 def hello_person():
-    return """
-        <p>What subreddit do you want to analyze?</p>
-        <form method="POST" action="%s"><input name="subreddit" /><input type="submit" value="Go!" /></form>
-        """ % (url_for('greet'),)
+    return render_template('results.html', showGraph=False)
 
-#TODO: error handling for nonexistent subreddit
-@app.route('/greet', methods=['POST'])
-def greet():
-    subreddit_data = models.pull_stats(request.form["subreddit"])
-    daychart_info = models.get_stats(subreddit_data["day"], "day")
-    hourchart_info = models.get_stats(subreddit_data["hour"], "hour")
-    # return """
-    #     <p>%s</p>
-    #     <p><a href="%s">Back to start</a></p>
-    #     """ % (greeting, url_for('hello_person'))
-    return render_template('results.html', subreddit=request.form["subreddit"], daychartInfo=daychart_info, hourchartInfo=hourchart_info)
+@app.route('/analysis', methods=['POST'])
+def analysis():
+    error = None
+
+    if request.form["subreddit"] == "":
+        error = "Please enter a subreddit."
+        return render_template('results.html', showGraph=False, error=error)
+
+    try:
+        subreddit_data = models.pull_stats(request.form["subreddit"])
+    except (praw.errors.NotFound, praw.errors.InvalidSubreddit):
+        error = "Could not retrieve data."
+        return render_template('results.html', showGraph=False, error=error)
+
+    daychart_info, best_day = models.get_stats(subreddit_data["day"], "day")
+    hourchart_info, best_hour = models.get_stats(subreddit_data["hour"], "hour")
+    best_hour = datetime.strptime(best_hour, "%H").strftime("%I %p")
+
+    top_subreddits = models.get_top_subreddits(10)
+    return render_template('results.html', subreddit=request.form["subreddit"], 
+        daychartInfo=daychart_info, hourchartInfo=hourchart_info, bestDay=best_day, bestHour=best_hour, showGraph=True,
+        topSubreddits=top_subreddits)
 
 #TODO: remove app.debug?
+#TODO: add readme
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key = "It's a secret"
     app.run()
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-import urllib, json
+# import urllib, json
 from datetime import datetime
 from numpy import median
 import time
 from collections import defaultdict
+import praw
 
 """
 Outputs the subreddit traffic stats in JSON format
@@ -11,24 +12,25 @@ The arrays in both the "hours" and "months" arrays are laid out like: [epoch tim
 """
 def pull_stats(subreddit):
     url = "https://www.reddit.com/r/" + subreddit + "/about/traffic.json"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
+    r = praw.Reddit(user_agent='DT Subreddit Activity Application')
+    data = r.request_json(url)
     
     return data
 
 """
-Converts epoch time to day of week or hour
+Converts epoch time to day of week or hour (in PST)
 """
 def get_time(ep, time_type):
+    pst_offset = 3600*8 # the difference between GMT and PST is -8 hours
     time_dict = {
         "day": "%A",
         "hour": "%H"
     }
 
-    return time.strftime(time_dict[time_type], time.gmtime(ep))
+    return time.strftime(time_dict[time_type], time.gmtime(ep - pst_offset))
 
 """
-Returns the median number of pageviews of each day of week as a dictionary
+Returns the median number of pageviews as a dictionary and the highest median value as a string
 """
 def get_stats(views_list, time_type):
     all_pageviews = defaultdict(lambda: [], {})
@@ -41,4 +43,14 @@ def get_stats(views_list, time_type):
     for i_time, pageviews_list in all_pageviews.iteritems():
         median_pageviews[i_time] = int(median(pageviews_list))
 
-    return median_pageviews
+    return median_pageviews, max(median_pageviews.keys(), key=(lambda k: median_pageviews[k]))
+"""
+Returns n most active subreddits
+"""
+def get_top_subreddits(n):
+    subreddit_list = []
+    r = praw.Reddit(user_agent='DT Subreddit Activity Application')
+    popular_subreddits = r.get_popular_subreddits(limit=n)
+    for subreddit in popular_subreddits:
+        subreddit_list.append(subreddit)
+    return subreddit_list
